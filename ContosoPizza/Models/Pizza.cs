abf7739ecf0294092d23b4ebd1cb36a50bc46c57
stantolovic <stantolovic@gmail.com>
2024-10-21 10:31:17 +0200

    Dodani apiji i servisi za CRUD Pizzas

--- ContosoPizza/Models/Pizza.cs
+++ ContosoPizza/Models/Pizza.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContosoPizza.Models;
 
 public class Pizza
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string? Name { get; set; }
 
     public Sauce? Sauce { get; set; }
